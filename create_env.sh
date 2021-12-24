19acb2e63e24b10992cf55fa8f37c1e12ffeb3b6
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-12-23 20:17:08 -0600

    sync

---
+++ create_env.sh
@@ -0,0 +1,19 @@
+conda create --name qd-acl python=3.6
+
+conda activate qd-acl
+
+pip install jupyterlab jupyterlab_vim
+
+pip install tensorflow==1.4.0
+
+cd ~/TeachMyAgent
+pip install -e .
+
+pip install gym==0.15.7
+
+pip install tianshou
+
+cd ~/TeachMyAgent
+pip install -e .
+
+
